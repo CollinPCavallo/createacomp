d57b708687f7d178ddeeeec43e2f9a31624a1301
Collin Cavallo <Collincavallo@gmail.com>
2018-06-30 15:56:37 -0500

    fixed styling on account nav and sidenav

--- public/app.js
+++ public/app.js
@@ -3,15 +3,18 @@ function openNav() {
     document.getElementById("mySidenav").style.height = "100%";
     // document.getElementsByClassName("navRight").style.height = "360px";
     document.getElementById("main").style.marginLeft = "500px";
-    document.getElementsByClassName('backArrow')[0].style.display = 'block';
+    document.getElementsByClassName('left')[0].style.display = 'block';
     document.getElementsByClassName('backArrow')[0].style.marginLeft = '25%';
-    document.body.style.backgroundColor = "rgba(34,33,33,.9)";
+    // document.body.style.backgroundColor = "rgba(34,33,33,.9)";
+    
     
 }
 function openAccount() {
-    document.getElementById("accountTab").style.width = "464px";
+    document.getElementById("accountTab").style.width = "24%";
     document.getElementById("main").style.marginRight = "500px";
-    document.body.style.backgroundColor = "rgba(34,33,33,.9)";
+    // document.body.style.backgroundColor = "rgba(34,33,33,.9)";
+    document.getElementsByClassName('right')[0].style.display = 'block';
+    document.getElementsByClassName('right')[0].style.marginLeft = '67%';
 }
 
 /* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
@@ -25,5 +28,6 @@ function closeNav() {
 function closeAccount() {
     document.getElementById("accountTab").style.width = "0";
     document.getElementById("main").style.marginRight = "0";
+    document.getElementsByClassName('right')[0].style.display = 'none';
     document.body.style.backgroundColor = "#222121";
 }
