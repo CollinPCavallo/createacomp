e7d153a13d8c3091e61135b57dd6b59f55d2a2f1
Collin Cavallo <collincavallo@gmail.com>
2018-07-11 11:22:54 -0500

    added update and edit routes added edit form next to switch to orm

--- app.js
+++ app.js
@@ -1,39 +1,41 @@
 var express = require('express'),
     mongoose = require('mongoose'),
-    bodyParser = require('body-parser');
+    mysql = require('mysql');
+    bodyParser = require('body-parser'),
+    methodOverride = require('method-override');
 
 var app = express();
 var port = 3000
-mongoose.connect('mongodb://localhost/create_a_comp')
+// mongoose.connect('mongodb://localhost/create_a_comp')
+var connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user:'root',
+    password:'root',
+    database: 'create_a_comp'
+    })
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log(`connected as id ${connection.threadId}`)
+})
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(methodOverride('_method'));
 
-var compSchema = new mongoose.Schema({
-    name: String,
-    author: String,
-    created: {type: Date, default: Date.now}
-})
-var Comp = mongoose.model('Comp', compSchema);
-
-// Comp.create({
-//     name: 'Test Pc',
-//     Author: 'Collin'
-// })
 
 app.get('/', function(req, res) {
     res.redirect('/builds')
 });
 //INDEX ROUTE
 app.get('/builds', function(req,res) {
- var comps = Comp.find({}, function(err, comps) {
-     if(err){
-         console.log(err)
-     } else {
-         res.render('index', {builds: comps})
-     }
- });
+
+     connection.query('select * from build', function (err, results) {
+        if (err) throw err
+        console.log(results)
+        res.render('index', {builds: results})
+     })
 });
 
 //NEW ROUTE
@@ -42,26 +44,68 @@ app.get('/build', function(req, res){
 });
 //CREATE ROUTE
 app.post('/builds', function(req, res) {
-    Comp.create(req.body.comp ,function(err, newComp) {
+
+    connection.query(
+        `insert into build set ?`,
+        {
+            name: req.body.comp.name,
+            author: req.body.comp.author
+        }
+        , function(err, results) {
         if (err) {
-            res.render('build')
+            console.log(err);
         } else {
             res.redirect('/builds');
-        };
+        }
     });
 });
 
 //SHOW ROUTE
 app.get('/build/:id', function(req, res) {
-    Comp.findById(req.params.id, function(err, foundBuild) {
-        if(err) {
-            console.log(err)
-        } else {
-            res.render('show', {build: foundBuild})
-        }
-    })
-});
 
+    connection.query(
+        'select * from build where ?', {
+            id: req.params.id
+        },function(err, result) {
+            if (err) {
+                console.log(err)
+            } else {
+                res.render('show', {build: result[0]})
+            }
+        })
+});
+//EDIT ROUTE
+app.get('/build/:id/edit' ,function (req, res) {
+    connection.query(
+        'select * from build where ?', {
+            id :req.params.id
+        }, function(err, foundBuild) {
+            if (err) {
+                console.log(err)
+            } else {
+                res.render('edit', {build : foundBuild[0]})
+            };
+        }
+    );
+});
+//UPDATE ROUTE
+app.put('/build/:id', function (req,res) {
+    
+
+    connection.query(
+        `update build set ? where id = ${req.params.id}`, {
+            name: req.body.comp.name,
+            author: req.body.comp.author,
+            
+        }, function (err, updatedBuild) {
+            if (err) {
+                console.log(err)
+            } else {
+                res.redirect(`/build/${req.params.id}`);
+            }
+        }
+    )
+})
 
 
 app.get('/search', function(req, res) {
